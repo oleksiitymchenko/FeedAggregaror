3076ef5d68a26607ec57383ee605aa7a993cbfa0
Oleksii Tymchenko <olexiytimchenko@gmail.com>
2018-09-23 18:20:15 +0300

    removed test project

---
+++ RSS_FeedAggregaror/FeedAggregator.Tests/FeedParserTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace FeedAggregator.Tests
+{
+    public class FeedParserTests
+    {
+        [Fact]
+        public void ParseRssFileReturnsListOfItems()
+        {
+
+        }
+    }
+}
